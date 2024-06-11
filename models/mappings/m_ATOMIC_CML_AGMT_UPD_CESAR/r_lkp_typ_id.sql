375c63e316fa6986593d61709abae012320dfff2
jkumar@komhar.com <jkumar@komhar.com>
2024-06-11 20:19:17 +0000

    Added files

---
+++ models/mappings/m_ATOMIC_CML_AGMT_UPD_CESAR/r_lkp_typ_id.sql
@@ -0,0 +1,6 @@
+{{ config(materialized="view") }}
+
+with typ as (select * from {{ source("", "typ") }})
+select typ_id, nm
+from (select a.typ_id as typ_id, a.nm as nm from typ a)
+qualify row_number() over (partition by nm order by typ_id) = 1
