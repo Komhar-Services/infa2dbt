346eff139fdd9a29e8061658025dc78970d90404
jkumar@komhar.com <jkumar@komhar.com>
2024-06-12 01:28:46 +0000

    comiting the changes

--- models/mappings/m_CDC_CC_USER/cdc_cc_user.sql
+++ models/mappings/m_CDC_CC_USER/cdc_cc_user.sql
@@ -1,8 +1,27 @@
+-- {{
+-- 	config(
+-- 		materialized = "incremental",
+-- post_hook = "UPDATE {{ var("dbownerprst") }}.dbaall.CDC_CC_USER TGT SET  LTST_REC_IND = 'N' where  TGT.ID in (select ID from {{ var("dbownerprst") }}.dbaall.CDC_CC_USER SRC where TGT.ID=SRC.ID AND SRC.BAT_ID = {{ var("batch_id") }} and SRC.LTST_REC_IND = 'Y') and TGT.BAT_ID <> {{ var("batch_id") }} and TGT.LTST_REC_IND = 'Y'"
+-- 	)
+-- }}
+
 {{
-	config(
-		materialized = "incremental",
-post_hook = "UPDATE {{ var("dbownerprst") }}.dbaall.CDC_CC_USER TGT SET  LTST_REC_IND = 'N' where  TGT.ID in (select ID from {{ var("dbownerprst") }}.dbaall.CDC_CC_USER SRC where TGT.ID=SRC.ID AND SRC.BAT_ID = {{ var("batch_id") }} and SRC.LTST_REC_IND = 'Y') and TGT.BAT_ID <> {{ var("batch_id") }} and TGT.LTST_REC_IND = 'Y'"
-	)
+    config(
+        materialized = "incremental",
+        post_hook = """
+            UPDATE {{ var('dbownerprst') }}.dbaall.CDC_CC_USER TGT 
+            SET LTST_REC_IND = 'N' 
+            WHERE TGT.ID IN (
+                SELECT ID 
+                FROM {{ var('dbownerprst') }}.dbaall.CDC_CC_USER SRC 
+                WHERE TGT.ID = SRC.ID 
+                AND SRC.BAT_ID = {{ var('batch_id') }} 
+                AND SRC.LTST_REC_IND = 'Y'
+            ) 
+            AND TGT.BAT_ID <> {{ var('batch_id') }} 
+            AND TGT.LTST_REC_IND = 'Y'
+        """
+    )
 }}
 
 with
