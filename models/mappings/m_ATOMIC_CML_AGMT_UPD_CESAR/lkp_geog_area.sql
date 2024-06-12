79dc116a21b9e5666e99fd3069c1ff2d72ec79cf
jkumar@komhar.com <jkumar@komhar.com>
2024-06-12 01:52:15 +0000

    deleting the objects

--- models/mappings/m_ATOMIC_CML_AGMT_UPD_CESAR/lkp_geog_area.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized="view") }}
-
-with geog_area as (select * from {{ source("", "geog_area") }})
-select plc_anchr_id, abbr, in_abbr
-from (select plc_anchr_id, abbr, in_abbr from geog_area)
-qualify row_number() over (partition by abbr order by plc_anchr_id) = 1
--- models/mappings/m_ATOMIC_CML_AGMT_UPD_CESAR/r_lkp_typ_id.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized="view") }}
-
-with typ as (select * from {{ source("", "typ") }})
-select typ_id, nm
-from (select a.typ_id as typ_id, a.nm as nm from typ a)
-qualify row_number() over (partition by nm order by typ_id) = 1
