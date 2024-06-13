cc8ac3421a054e9db25af958af2b3faa1e6aa7b3
jkumar@komhar.com <jkumar@komhar.com>
2024-06-13 05:14:56 +0000

    Join With Row_Number

--- models/znaw_ifrs_pdo/m_wrk_ifrs_lrc_ffaa_fctr/wrk_fctr_err.sql
+++ models/znaw_ifrs_pdo/m_wrk_ifrs_lrc_ffaa_fctr/wrk_fctr_err.sql
@@ -142,7 +142,8 @@ with
             null as fctr_typ14,
             null as fctr14,
             null as fctr_typ15,
-            null as fctr15
+            null as fctr15,
+            ROW_NUMBER() OVER(order by null) rn
         from exp_set_val
     ),
     mplt_fctr_tlrnc_chk as (
@@ -160,8 +161,9 @@ with
             mplt_fctr_tlrnc_chk.err_desc,
             exp_set_val.eff_to_tistmp as eff_to_tistmp2,
             exp_set_val.orgl_port_cd,
-            exp_set_val.eff_fm_tistmp
-        from exp_set_val
+            exp_set_val.eff_fm_tistmp,
+            rn
+        from exp_set_val,mplt_fctr_tlrnc_chk
         -- Manually join with mplt_FCTR_TLRNC_CHK
         where ovrrdn_flg = 'Y'
     ),
@@ -184,7 +186,8 @@ with
             {{ var("batch_id") }} as bat_id,
             current_timestamp as out_eff_fm_tistmp,
             orgl_port_cd,
-            eff_fm_tistmp
+            eff_fm_tistmp,
+            rn
         from filtar_tolerance
     )
 select
@@ -203,3 +206,4 @@ select
     updt_pop_info_id,
     bat_id
 from exp_set_err_val
+inner join mplt_fctr_tlrnc_chk using(rn)
